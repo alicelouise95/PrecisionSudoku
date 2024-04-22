27e1bc218512687053475ecaf9956190dfeea3f4
alice <alicelouisew95@gmail.com>
2024-04-22 15:01:49 +0200

    Google Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.alicelouisew.sudooooku"
+    namespace = "com.alicelouisew.PrecisionSudoku"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.alicelouisew.sudooooku"
+        applicationId = "com.alicelouisew.PrecisionSudoku"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Sudooooku"
+rootProject.name = "Precision Sudoku"
 include(":app")
  
